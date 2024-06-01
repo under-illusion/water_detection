887dd5bd7e82bd195c668169975735d60554240e
under-illusion <100740937+under-illusion@users.noreply.github.com>
2024-06-01 14:59:06 +0800

    Update RF24.ino

--- RF24.ino
+++ RF24.ino
@@ -1,20 +1,12 @@
-/*
- * See documentation at https://nRF24.github.io/RF24
- * See License information at root directory of this library
- * Author: Brendan Doherty (2bndy5)
- */
-
 #include <SPI.h>
-#include "printf.h"
 #include "RF24.h"
 
 #define CE_PIN 7
 #define CSN_PIN 8
-// instantiate an object for the nRF24L01 transceiver
 
 struct SensorData {
   float conductivity;
-  //float level;
+  float level;
   float turbidity;
 };
 
@@ -22,11 +14,13 @@ SensorData readSensors() {
   SensorData data;
   
   // 读取电导率传感器的值（模拟输入）
-  float compensationVolatge = analogRead(A0)*(5.0 / 1024.0);
-  data.conductivity = ((133.42*compensationVolatge*compensationVolatge*compensationVolatge - 255.86*compensationVolatge*compensationVolatge + 857.39*compensationVolatge)*0.5*1.8);
+  float compensationVolatge = analogRead(A0) * (5.0 / 1024.0);
+  data.conductivity = ((133.42 * compensationVolatge * compensationVolatge * compensationVolatge - 
+                        255.86 * compensationVolatge * compensationVolatge + 
+                        857.39*compensationVolatge) * 0.5 * 1.8);
   
   // 读取水位传感器的值（数字输入）
-  //data.level = digitalRead(A1);
+  data.level = digitalRead(A1);
   
   // 读取浊度传感器的值
   float sensorValue = analogRead(A2);
@@ -37,24 +31,9 @@ SensorData readSensors() {
 
 RF24 radio(CE_PIN, CSN_PIN);
 
-// Let these addresses be used for the pair
 uint8_t address[][6] = { "1Node", "2Node" };
-// It is very helpful to think of an address as a path instead of as
-// an identifying device destination
-
-// to use different addresses on a pair of radios, we need a variable to
-// uniquely identify which address this radio will use to transmit
-bool radioNumber = 0;  // 0 uses address[0] to transmit, 1 uses address[1] to transmit
-
-// Used to control whether this node is sending or receiving
-bool role = true;  // true = TX role, false = RX role
-
-// For this example, we'll be using a payload containing
-// a single float number that will be incremented
-// on every successful transmission
 
 void setup() {
-
   Serial.begin(115200);
   while (!Serial) {
     // some boards need to wait to ensure access to serial over USB
@@ -66,57 +45,27 @@ void setup() {
     while (1) {}  // hold in infinite loop
   }
   
-  // print example's introductory prompt
-  Serial.println(F("RF24/examples/GettingStarted"));
-  
-  // To set the radioNumber via the Serial monitor on startup
-  Serial.println(F("Which radio is this? Enter '0' or '1'. Defaults to '0'"));
-  
-  Serial.print(F("radioNumber = "));
-  Serial.println((int)radioNumber);
+  Serial.println(F("Prepared to send data."));
+  radio.setPALevel(RF24_PA_LOW);
 
-  // role variable is hardcoded to RX behavior, inform the user of this
-  Serial.println(F("*** PRESS 'T' to begin transmitting to the other node"));
+  radio.setPayloadSize(sizeof(SensorData));
 
-  // Set the PA Level low to try preventing power supply related problems
-  // because these examples are likely run with nodes in close proximity to
-  // each other.
-  radio.setPALevel(RF24_PA_LOW);  // RF24_PA_MAX is default.
+  radio.openWritingPipe(address[0]);
 
-  // save on transmission time by setting the radio to only transmit the
-  // number of bytes we need to transmit a float
-  radio.setPayloadSize(sizeof(SensorData));  // float datatype occupies 4 bytes
+  radio.openReadingPipe(1, address[1]);
 
-  // set the TX address of the RX node into the TX pipe
-  radio.openWritingPipe(address[radioNumber]);  // always uses pipe 0
-
-  // set the RX address of the TX node into a RX pipe
-  radio.openReadingPipe(1, address[!radioNumber]);  // using pipe 1
-
-  // additional setup specific to the node's role
-  if (role) {
-    radio.stopListening();  // put radio in TX mode
-  } else {
-    radio.startListening();  // put radio in RX mode
-  }
-  
-  // For debugging info
-  // printf_begin();             // needed only once for printing details
-  // radio.printDetails();       // (smaller) function that prints raw register values
-  // radio.printPrettyDetails(); // (larger) function that prints human readable data
-
-}  // setup
+  radio.stopListening();  // put radio in TX mode
+}
 
 void loop() {
-  if (role) {
-    // This device is a TX node
+
     Serial.print(F("TX !!!!!! "));
+
     SensorData sensorData = readSensors();
     unsigned long start_timer = micros();                // start the timer
     bool report = radio.write(&sensorData, sizeof(SensorData));  // transmit & save the report
     unsigned long end_timer = micros();                  // end the timer
-    
-    
+
     if (report) {
       Serial.print(F("Transmission successful! "));  // payload was delivered
       Serial.print(F("Time to transmit = "));
@@ -125,17 +74,13 @@ void loop() {
       
       Serial.print("conductivity:");
       Serial.println(sensorData.conductivity);
-      //Serial.print("level:");
-      //Serial.println(sensorData.level);
+      Serial.print("level:");
+      Serial.println(sensorData.level);
       Serial.print("turbidity:");
       Serial.println(sensorData.turbidity);
     } else {
       Serial.println(F("Transmission failed or timed out"));  // payload was not delivered
     }
-
     // to make this example readable in the serial monitor
     delay(1000);  // slow transmissions down by 1 second
-
-  } 
-
-}  // loop
+}
