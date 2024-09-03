4b4ed522e38794d4b1720636ebbe1353e5b3b9af
under-illusion <100740937+under-illusion@users.noreply.github.com>
2024-09-03 14:31:03 +0800

    Update new_version_recevier.py

--- new_version_recevier.py
+++ new_version_recevier.py
@@ -1,4 +1,11 @@
 from utilities import RF_recevier
+TX_adress = [b"1Node", b"2Node"]
 
-receiver = RF_recevier()
-receiver.listen()
+if __name__ == "__main__":
+    try:
+        receiver = RF_recevier(address=TX_adress)
+        while True:
+            receiver.listen()
+    except KeyboardInterrupt:
+        print("Keyboard interrupt detected. Exiting...")
+        receiver.radio.powerDown()
